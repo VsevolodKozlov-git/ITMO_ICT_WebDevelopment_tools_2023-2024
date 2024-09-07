474f018b63b4caf3ddb4fa629b04eb8e95ac52e3
Vsevolod Kozlov <71010414+VsevolodKozlov-git@users.noreply.github.com>
2024-09-07 10:03:44 +0300

    Перешел на jwt с jose

--- task1/LaboratoryPart/auth.py
+++ task1/LaboratoryPart/auth.py
@@ -1,6 +1,5 @@
 from passlib.context import CryptContext
-from jose import jwt
-from jose.exceptions import ExpiredSignatureError, JWTError
+import jwt
 from pathlib import Path
 from dotenv import dotenv_values
 import datetime
@@ -45,11 +44,11 @@ def generate_token(username):
 def decode_token(token):
     try:
         decoded_token = jwt.decode(token, SECRET_KEY, ALGORITHM)
-    except ExpiredSignatureError:
+    except jwt.exceptions.ExpiredSignatureError:
         raise HTTPException(
             status_code=403, detail="Your token expired. Create a new one"
         )
-    except JWTError:
+    except jwt.exceptions.InvalidTokenError:
         raise HTTPException(status_code=403, detail="Invalid token. Invalid format")
     return decoded_token
 
